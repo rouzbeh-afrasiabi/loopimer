eaa6bc475c2b5bf5b4f7989b5cf1dbbfbc6257f0
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-07-18 23:24:52 -0400

    Update lp.py

--- loopimer/lp.py
+++ loopimer/lp.py
@@ -194,7 +194,7 @@ class loopi:
         loop_trigger.join()
         
         
-class loopimer:
+class loopimerx:
     def __init__(self, *args,**kwargs):
         self.kwargs=kwargs
         self.ltimer=loopi(**self.kwargs)
