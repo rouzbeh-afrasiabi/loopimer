137ac57c78edc7f321d140943c8e11b882d1ff99
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-07-19 08:18:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rouzbeh-afrasiabi/loopimer",
     download_url="https://github.com/rouzbeh-afrasiabi/loopimer/archive/v1.0.1.tar.gz",
-    keywords = ['time', 'loop', 'control','threading','function']
+    keywords = ['time', 'loop', 'control','threading','function'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
