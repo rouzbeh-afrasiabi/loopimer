55882b26063b997bc3c23304494f1f78e4ccee27
Rouzbeh Afrasiabi <34456532+rouzbeh-afrasiabi@users.noreply.github.com>
2019-07-24 10:04:14 -0400

    New version released V1.0.2

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="loopimer",
-    version="1.0.1",
+    version="1.0.2",
     author="Rouzbeh Afrasiabi",
     author_email="rouzbeh.afrasiabi@gmail.com",
     description="Package for time-controlled consecutive execution of a  function using threading.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rouzbeh-afrasiabi/loopimer",
-    download_url="https://github.com/rouzbeh-afrasiabi/loopimer/archive/v1.0.1.tar.gz",
+    download_url="https://github.com/rouzbeh-afrasiabi/loopimer/archive/v.1.0.2.tar.gz",
     keywords = ['time', 'loop', 'control','threading','function'],
     packages=setuptools.find_packages(),
     classifiers=[
